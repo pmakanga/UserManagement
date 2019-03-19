cbc45ed3baa3edbd51c4c8727903d5cb6c6f20f7
Patrick <pmakanga@gmail.com.com>
2019-03-19 23:05:05 +0300

    Security with JWT token Authentication

---
+++ UserManagement.API/Controllers/AuthController.cs
@@ -0,0 +1,81 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using UserManagement.API.Data;
+using UserManagement.API.DTOs;
+using UserManagement.API.Model;
+
+namespace UserManagement.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository _repo;
+        private readonly IConfiguration _config;
+        public AuthController(IAuthRepository repo, IConfiguration config)
+        {
+            _config = config;
+            _repo = repo;
+
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(UserForRegisterDTO userForRegisterDTO)
+        {
+            // validate request
+
+            userForRegisterDTO.UserName = userForRegisterDTO.UserName.ToLower();
+
+            if (await _repo.UserExists(userForRegisterDTO.UserName))
+                return BadRequest("UserName already exists");
+            var userToCreate = new User
+            {
+                UserName = userForRegisterDTO.UserName
+            };
+
+            var createdUser = await _repo.Register(userToCreate, userForRegisterDTO.Password);
+
+            return StatusCode(201);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(UserForLoginDTO userForLoginDTO)
+        {
+            var userFromRepo = await _repo.Login(userForLoginDTO.UserName, userForLoginDTO.Password);
+            if (userFromRepo == null)
+                return Unauthorized();
+
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
+                new Claim(ClaimTypes.Name, userFromRepo.UserName)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config.GetSection("AppSettings:Token").Value));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = creds
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return Ok(new {
+                token = tokenHandler.WriteToken(token),
+            });
+            
+        }
+    }
+}
---
+++ UserManagement.API/DTOs/UserForLoginDTO.cs
@@ -0,0 +1,8 @@
+namespace UserManagement.API.DTOs
+{
+    public class UserForLoginDTO
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
